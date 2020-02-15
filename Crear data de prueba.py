98a5c6cf63634a4419f2aad36874d6fa3a8a4b68
Yadira Quinchiguango <33789163+lilyquinchiguango@users.noreply.github.com>
2020-02-15 06:07:00 -0500

    Columnas

--- Crear data de prueba.py
+++ Crear data de prueba.py
@@ -9,20 +9,26 @@ Created on Thu Feb 13 05:29:18 2020
 #GRAFICOS ESTADISTICOS CON MATPLOTLIB
 #TEMA: VIDEOJUEGOS
 from faker import Faker
-from faker.providers import internet
-from faker_credit_score import CreditScore #usar para crear socre de juegos Fuente: https://faker.readthedocs.io/en/stable/providers.html
+#from faker.providers import internet
+#from faker_credit_score import CreditScore #usar para crear socre de juegos Fuente: https://faker.readthedocs.io/en/stable/providers.html
 import pandas as pd
+import random
 fake = Faker()
 
 
-################################333333
-#numero de filas de la base a crear
+################################
+# Numero de filas de la base a crear
 numero_casos=100000
 
-fecha=[]
-usuario =[]
-puntaje =[]
-juego =[]
+fecha = []
+usuario = []
+puntaje = []
+juego = []
+tipo_juego = []
+tiempo_juego = []
+posicion_jugador = []
+nivel = []
+
 for _ in range(numero_casos):
     #crea fechas de prueba
     fecha.append(fake.date(pattern='%Y-%m-%d', end_datetime=None))
@@ -32,8 +38,21 @@ for _ in range(numero_casos):
     puntaje.append(fake.credit_score_provider())
     #crea nombre de juego # reemplazar los nombres despues
     juego.append(fake.credit_score_provider())
-    
+    #crea usuarios de prueba
+    tipo_juego.append(random.choise(["Aventura","Arcade","Estrategia"]))
+    #crea usuarios de prueba
+    tiempo_juego.append(random.random())
+    #crea usuarios de prueba
+    posicion_jugador.append(random.choise(["Primero","Segundo","Tercero"]))
+    #crea usuarios de prueba
+    nivel.append(random.choise(["Facil","Intermedio","Dificil"]))
 
 
-frame = { 'fecha': pd.Series(fecha), 'usuario': pd.Series(usuario), 'puntaje':pd.Series(puntaje), 'juego':pd.Series(juego) } 
-df = pd.DataFrame(frame) #USAR ESTE DATAFRAMA PARA LOS GRAFICOS
+frame = { 'fecha': pd.Series(fecha), 'usuario': pd.Series(usuario), 'puntaje':pd.Series(puntaje), 'juego':pd.Series(juego), 'tipo_juego':pd.Series(tipo_juego), 'tiempo_juego':pd.Series(tiempo_juego), 'posicion_jugador':pd.Series(posicion_jugador), 'nivel':pd.Series(nivel)} 
+df = pd.DataFrame(frame) #USAR ESTE DATAFRAME PARA LOS GRAFICOS
+
+
+
+
+
+
